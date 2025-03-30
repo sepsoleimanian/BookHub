203c3c45102474a2e93efb5eb2e917bb20289718
Sepideh Soleimanian <sepsoleimanian@gmail.com>
2025-03-31 00:09:46 +0330

    update repo

--- books_v2.py
+++ books_v2.py
@@ -31,7 +31,7 @@ class BookRequest(BaseModel):
     published_date: int = Field(gt=1999, lt=2031)
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             'example': {
                 'title': 'A new book',
                 'author': 'Coding With Roby',
